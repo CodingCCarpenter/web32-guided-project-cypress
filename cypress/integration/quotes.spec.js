49e9a252f15fcb5a199635886d16f6732f0ea3e0
CodingCCarpenter <Christineassists@aol.com>
2020-06-18 11:37:53 -0500

    inserting comments to annotate the anatomy of the first test

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,7 +1,10 @@
 // write tests here
 describe('Inputs and a submit button', () => {
     it('can navigate to the site', () => {
+        // remote control operation
         cy.visit('http://localhost:1234')
+
+        // assertion to test something from the rco
         cy.url().should('include', 'http://localhost:1234')
     })
 })
