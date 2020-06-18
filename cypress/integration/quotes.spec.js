2771a1904d4c9205823279476b31e50a5c919dfb
CodingCCarpenter <Christineassists@aol.com>
2020-06-18 11:36:06 -0500

    adding assertion to inputs and submit button test

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -2,5 +2,8 @@
 describe('Inputs and a submit button', () => {
     it('can navigate to the site', () => {
         cy.visit('http://localhost:1234')
+        cy.url().should('include', 'http://localhost:1234')
     })
 })
+
+
